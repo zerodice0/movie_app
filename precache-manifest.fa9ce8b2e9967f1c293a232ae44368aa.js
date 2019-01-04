803ca5843e6cbe2cc8c1066a66c4ef056956cbd5
zerodice0 <zerodice0@gmail.com>
2019-01-04 15:43:11 +0900

    first commit

---
+++ precache-manifest.fa9ce8b2e9967f1c293a232ae44368aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "9c2d102a26cb5a65fa1d",
+    "url": "/movie_app/static/js/main.9c2d102a.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/movie_app/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "9c2d102a26cb5a65fa1d",
+    "url": "/movie_app/static/css/main.578bd135.chunk.css"
+  },
+  {
+    "revision": "f384db68786a3f1979d4f41a2d5ada1f",
+    "url": "/movie_app/index.html"
+  }
+];
